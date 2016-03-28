ca35d4f179ebb311f1a2b72511eebb711245eb8f
Qing Zhang <peachovo@gmail.com>
2016-03-28 22:33:53 +0800

    写完验证功能
    清空输入后自动恢复灰色提示的功能还有待完善

--- task0002/29/task29.js
+++ task0002/29/task29.js
@@ -2,9 +2,9 @@ function check (e, input) {
 	var inputBox = document.getElementById('input');
 	var input = inputBox.value;
 	var message = document.getElementById('message');
-	console.log(typeof(input));
-	if (input.length <= 4 || input.length >= 16) {
-		message.textContent = '姓名不能为空';
+
+	if (input.length < 4 || input.length > 16) {
+		message.textContent = '请输入长度为4~16个字符的名称，英文字母占1个字符，汉字占2个字符';
 		message.style.color = 'red';
 		inputBox.style.borderColor = 'red';
 	} else {
@@ -16,6 +16,21 @@ function check (e, input) {
 	e.stopPropagation();
 }
 
+function focusMsg (e) {
+	var inputBox = document.getElementById('input');
+	var input = inputBox.value;
+	if (input.length === 0) {
+		message.textContent = '必填，长度为4~16个字符';
+		message.style.color = '#A9A9A9';
+		inputBox.style.borderColor = '#eee';
+	}
+	 e.preventDefault();
+	 e.stopPropagation();
+}
+
 // 绑定事件
 var submitBtn = document.getElementById('submitBtn');
 submitBtn.addEventListener('click', check, false);
+
+var inputBox = document.getElementById('input');
+inputBox.addEventListener('focus', focusMsg, false);
