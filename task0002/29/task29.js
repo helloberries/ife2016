b60c85f209010f3de31470cf6ed1e19dc5b19d0d
Qing Zhang <peachovo@gmail.com>
2016-03-28 22:20:20 +0800

    写完验证功能
    还缺少删除输入，自动恢复到初始状态的功能

--- task0002/29/task29.js
+++ task0002/29/task29.js
@@ -3,10 +3,14 @@ function check (e, input) {
 	var input = inputBox.value;
 	var message = document.getElementById('message');
 	console.log(typeof(input));
-	if (input.length <= 4) {
+	if (input.length <= 4 || input.length >= 16) {
 		message.textContent = '姓名不能为空';
 		message.style.color = 'red';
 		inputBox.style.borderColor = 'red';
+	} else {
+		message.textContent = '名称格式正确';
+		message.style.color = 'green';
+		inputBox.style.borderColor = 'green';
 	}
 	e.preventDefault();
 	e.stopPropagation();
